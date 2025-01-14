57f9fbbf3758afa23548c58ad3e48e6acc1d8ef8
“sihamghb” <sihambenarbia2.com>
2025-01-14 22:38:55 +0100

    arrange route and assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {DressComponent} from './dress/dress.component';
+import {InscriptionComponent} from './inscription/inscription.component';
+import {LoginComponent} from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path :'home', component :HomeComponent},
+  {path: 'dress', component: DressComponent},
+  { path: 'inscription', component: InscriptionComponent},
+  { path:'login', component: LoginComponent},
+  //{ path:'purchase', component:PurchaseComponent}, TODO : Ajouter composant pour le panier
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  { path:'**', component: HomeComponent}
+];
