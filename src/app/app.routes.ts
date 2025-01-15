d620bc95e3536d0360328a3f916a8798dfb9433f
tesnime94 <tesnimeben94@yahoo.fr>
2025-01-15 20:03:11 +0100

    Inscription form html + css OK

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import {HomeComponent} from './home/home.component';
 import {DressComponent} from './dress/dress.component';
 import {InscriptionComponent} from './inscription/inscription.component';
 import {LoginComponent} from './login/login.component';
+import {AddDressComponent} from './add-dress/add-dress.component';
 
 export const routes: Routes = [
   {path :'home', component :HomeComponent},
+  {path: 'addDress', component: AddDressComponent},
   {path: 'dress', component: DressComponent},
   { path: 'inscription', component: InscriptionComponent},
   { path:'login', component: LoginComponent},
--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule], // Ajout des modules nécessaires
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
 export class InscriptionComponent {
+  public registerForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    surname: new FormControl('', Validators.required),
+    birth: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required),
+    phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
+  public onSubmit(): void {
+    // Affiche les valeurs du formulaire dans la console
+    console.log(this.registerForm.value);
+  }
 }
